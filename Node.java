3fb58a27d3be744ecc11d7fd099095895773ddda
Philip-W <Philip-W@users.noreply.github.com>
2015-09-06 23:09:36 +0100

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,12 +5,19 @@ private class Node {
   private Object data;
   Node next;
   
-  public Node(Object data){
-      
-      
+  public Node(Object o){
+      next = null;
+      data = o;
+  }      
+
+  public Node(Object o, Node nextVal){
+    next = nextVal;
+    data = o;
   }
   
+  public Object getData() { return data; }
+    
   
   
   
-}
+  }
