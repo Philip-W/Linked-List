f2fec35f860e291ee21cc0e6a4166faa7873a3b3
Philip-W <Philip-W@users.noreply.github.com>
2015-09-06 22:39:49 +0100

    Create linkList.java

---
+++ linkList.java
@@ -0,0 +1,16 @@
+
+public class linkList{
+
+  private Node head;
+  private int size;
+
+  public linkList(){
+    head = new Node(null);
+    size = 0;
+  }
+
+  public addNode(Object o){}
+
+  public addNode(Object o, int index){}
+  
+  public removeNode(int index){}
