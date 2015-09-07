b224d536450034f16629773a3360e5bf8c550ae6
Philip-W <philipgwoods90@gmail.com>
2015-09-07 13:29:54 +0100

    Revert "Revert "Create testLink.java""
    This reverts commit e736e552d0fdfaecb6d91ea3a11e3121404db405.

---
+++ testLink.java
@@ -0,0 +1,21 @@
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class testLink{
+
+  public static void main(String[] args){
+      linkList list = new linkList();
+
+      list.addToTail(5);
+      list.addToTail(7);
+      list.addToTail(19);
+
+      System.out.println(list);
+
+  }
+
+
+}
