95b3f12297e5c97001fe16b6e2d473a005410eb2
Philip-W <Philip-W@users.noreply.github.com>
2015-09-08 15:16:31 +0100

    Update testLink.java

--- testLink.java
+++ testLink.java
@@ -8,12 +8,22 @@ public class testLink{
 
   public static void main(String[] args){
       linkList list = new linkList();
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
 
       list.addToTail(5);
       list.addToTail(7);
       list.addToTail(19);
+      list.addToTail(25);
+      list.addToTail(12);
 
       System.out.println(list);
+
+      System.out.println("testing remove Node");
+
+      list.removeNode(n);
+      System.out.println(list);
+
 
   }
 
